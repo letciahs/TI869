b71fda60d9bdaabdfe6924ab404704b761b50266
Leticia Haussmam <lhaussmam@tic.local>
2023-09-20 14:36:21 +0100

    projeto com macros trignometria

---
+++ projeto/main.c
@@ -0,0 +1,15 @@
+#define PI 3.1415
+#define areaCirculo(r) ((PI)*(r)*(r))
+#define perimetroCirculo(r) (2*(PI)*(r))
+#define areaQuadrado(l) ((l)*(l)))
+#define perimetroQuadrado(l) (4*(l))
+#define areaRetangulo(a,b) ((a)*(b))
+#define perimetroRetangulo(a,b) ((2*(a))+(2*(b)))
+#define areaTriangulo(b,h) (((b)*(h))/2)
+#define perimetroTriangulo(b,c1,c2) ((b)+(c1)+(c2))
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
